dec06d67e669fb5a78f8b3cc8728100065298f89
undefinednulla <buihahaidanggg@gmail.com>
2021-12-26 13:29:20 +0700

    404

--- assets/script/app.js
+++ assets/script/app.js
@@ -7,10 +7,15 @@ const app = {
             if (searchValue != '') {
                 let data = await fetch(apiURL).then((data) => data.json())
                 console.log(data)
-                document.getElementById('city--name').textContent = data.name
-                document.getElementById('city--temperature').textContent = Math.round(data.main.temp - 273.15) + '°C'
-                document.getElementById('humidity').textContent = data.main.humidity + '%'
-                document.getElementById('windspeed').textContent = data.wind.speed + ' km/h'
+                if (data.cod == 200) {
+                    document.getElementById('city--name').textContent = data.name
+                    document.getElementById('city--temperature').textContent = Math.round(data.main.temp - 273.15) + '°C'
+                    document.getElementById('humidity').textContent = data.main.humidity + '%'
+                    document.getElementById('windspeed').textContent = data.wind.speed + ' km/h'
+                } else if (data.cod == 404) {
+                    document.getElementById('app-result').textContent = `Không tìm thấy ${searchValue}`
+                    document.getElementById('app-result').setAttribute('style', 'color: #F05454')
+                }
             }
         }
         document.getElementById('search').addEventListener('keypress', (e) => {
